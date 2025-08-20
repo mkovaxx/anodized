9b625bb726a4db30d37a32cd2d4b232bdd4a92dc
Mate Kovacs <mkovaxx@gmail.com>
2025-08-20 11:37:55 +0900

    add test to demonstrate problem with current approach to parsing closures

---
+++ tests/pattern_in_closure.rs
@@ -0,0 +1,17 @@
+use anodized::contract;
+
+#[contract(
+    ensures: |(a, b)| a <= b,
+    ensures: |(a, b)| (a, b) == pair || (b, a) == pair,
+)]
+fn sort_pair(pair: (i32, i32)) -> (i32, i32) {
+    let (a, b) = pair;
+    // Deliberately wrong implementation to trigger the contract.
+    (b, a)
+}
+
+#[test]
+#[should_panic(expected = "Postcondition failed: |(a, b)| a <= b")]
+fn test_sort_fail_postcondition() {
+    sort_pair((2, 5));
+}
