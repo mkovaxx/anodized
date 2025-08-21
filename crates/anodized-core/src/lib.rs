0f811234075b099be9f62bf7f39b6bf47c105441
Mate Kovacs <mkovaxx@gmail.com>
2025-08-21 23:12:10 +0900

    refactor: unify all Contract arguments into ContractArg

--- crates/anodized-core/src/lib.rs
+++ crates/anodized-core/src/lib.rs
@@ -25,28 +25,40 @@ impl TryFrom<ContractArgs> for Contract {
     type Error = syn::Error;
 
     fn try_from(args: ContractArgs) -> Result<Self> {
+        let mut binds_pattern: Option<&Pat> = None;
         let mut requires: Vec<Expr> = vec![];
         let mut maintains: Vec<Expr> = vec![];
         let mut ensures: Vec<ExprClosure> = vec![];
 
+        for arg in &args.items {
+            if let ContractArg::Binds { pattern } = arg {
+                if binds_pattern.is_some() {
+                    return Err(syn::Error::new(
+                        pattern.span(),
+                        "duplicate `binds` parameter",
+                    ));
+                }
+                binds_pattern = Some(pattern);
+            }
+        }
+
         // The default pattern for `ensures` conditions. It must be resolvable at the call site.
-        let default_output_pat = args
-            .binds_pat
-            .clone()
+        let default_closure_pattern = binds_pattern
             .map(|p| p.to_token_stream())
             .unwrap_or_else(|| quote! { output });
 
-        for condition in args.conditions {
-            match condition {
-                Condition::Requires { predicate } => requires.push(predicate),
-                Condition::Maintains { predicate } => maintains.push(predicate),
-                Condition::Ensures { predicate } => {
+        for arg in args.items {
+            match arg {
+                ContractArg::Requires { expr: predicate } => requires.push(predicate),
+                ContractArg::Maintains { expr: predicate } => maintains.push(predicate),
+                ContractArg::Ensures { expr: predicate } => {
                     // Convert a simple expression into a closure.
                     let closure: ExprClosure =
-                        syn::parse_quote! { |#default_output_pat| #predicate };
+                        syn::parse_quote! { |#default_closure_pattern| #predicate };
                     ensures.push(closure);
                 }
-                Condition::EnsuresClosure { closure } => ensures.push(closure),
+                ContractArg::EnsuresClosure { closure } => ensures.push(closure),
+                ContractArg::Binds { .. } => {}
             }
         }
 
@@ -60,98 +72,64 @@ impl TryFrom<ContractArgs> for Contract {
 
 /// A container for all parsed arguments from the `#[contract]` attribute.
 pub struct ContractArgs {
-    pub conditions: Vec<Condition>,
-    pub binds_pat: Option<Pat>,
-}
-
-/// Represents a single contract condition, e.g., `requires: x > 0`.
-pub enum Condition {
-    Requires { predicate: Expr },
-    Ensures { predicate: Expr },
-    EnsuresClosure { closure: ExprClosure },
-    Maintains { predicate: Expr },
+    pub items: Vec<ContractArg>,
 }
 
 impl Parse for ContractArgs {
     /// Custom parser for the contents of `#[contract(...)]`.
     fn parse(input: ParseStream) -> Result<Self> {
-        let mut conditions = Vec::new();
-        let mut binds_pat = None;
-
         // The arguments are a comma-separated list of conditions or a `binds` setting.
-        let items = Punctuated::<ContractArgItem, Token![,]>::parse_terminated(input)?;
-
-        for item in items {
-            match item {
-                ContractArgItem::Condition(condition) => conditions.push(condition),
-                ContractArgItem::Binds(pat) => {
-                    if binds_pat.is_some() {
-                        return Err(syn::Error::new(pat.span(), "duplicate `binds` setting"));
-                    }
-                    binds_pat = Some(pat);
-                }
-            }
-        }
+        let items = Punctuated::<ContractArg, Token![,]>::parse_terminated(input)?;
 
         Ok(ContractArgs {
-            conditions,
-            binds_pat,
+            items: items.into_iter().map(|x| x).collect(),
         })
     }
 }
 
 /// An intermediate enum to help parse either a condition or a `binds` setting.
-pub enum ContractArgItem {
-    Condition(Condition),
-    Binds(Pat),
+pub enum ContractArg {
+    Requires { expr: Expr },
+    Ensures { expr: Expr },
+    EnsuresClosure { closure: ExprClosure },
+    Maintains { expr: Expr },
+    Binds { pattern: Pat },
 }
 
-impl Parse for ContractArgItem {
+impl Parse for ContractArg {
     fn parse(input: ParseStream) -> Result<Self> {
         let lookahead = input.lookahead1();
         if lookahead.peek(kw::binds) {
-            // Parse `binds: pat`
+            // Parse `binds: <pat>`
             input.parse::<kw::binds>()?;
             input.parse::<Token![:]>()?;
-            let pat = Pat::parse_single(input)?;
-            Ok(ContractArgItem::Binds(pat))
-        } else if lookahead.peek(kw::requires)
-            || lookahead.peek(kw::ensures)
-            || lookahead.peek(kw::maintains)
-        {
-            // Parse a condition like `requires: predicate` or `ensures: |val| predicate`
-            Ok(ContractArgItem::Condition(input.parse()?))
-        } else {
-            Err(lookahead.error())
-        }
-    }
-}
-
-impl Parse for Condition {
-    /// Parses a single condition.
-    fn parse(input: ParseStream) -> Result<Self> {
-        let lookahead = input.lookahead1();
-        if lookahead.peek(kw::requires) {
+            Ok(ContractArg::Binds {
+                pattern: Pat::parse_single(input)?,
+            })
+        } else if lookahead.peek(kw::requires) {
+            // Parse `requires: <expr>`
             input.parse::<kw::requires>()?;
             input.parse::<Token![:]>()?;
-            Ok(Condition::Requires {
-                predicate: input.parse()?,
+            Ok(ContractArg::Requires {
+                expr: input.parse()?,
+            })
+        } else if lookahead.peek(kw::maintains) {
+            // Parse `maintains: <expr>`
+            input.parse::<kw::maintains>()?;
+            input.parse::<Token![:]>()?;
+            Ok(ContractArg::Maintains {
+                expr: input.parse()?,
             })
         } else if lookahead.peek(kw::ensures) {
+            // Parse `ensures: <expr>`
             input.parse::<kw::ensures>()?;
             input.parse::<Token![:]>()?;
             let predicate: Expr = input.parse()?;
             if let Expr::Closure(closure) = predicate {
-                Ok(Condition::EnsuresClosure { closure })
+                Ok(ContractArg::EnsuresClosure { closure })
             } else {
-                Ok(Condition::Ensures { predicate })
+                Ok(ContractArg::Ensures { expr: predicate })
             }
-        } else if lookahead.peek(kw::maintains) {
-            input.parse::<kw::maintains>()?;
-            input.parse::<Token![:]>()?;
-            Ok(Condition::Maintains {
-                predicate: input.parse()?,
-            })
         } else {
             Err(lookahead.error())
         }
