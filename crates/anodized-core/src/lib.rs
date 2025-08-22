b1c8db70ec722f9cdee771e0f045e9c6e6095cd3
Mate Kovacs <mkovaxx@gmail.com>
2025-08-22 14:50:56 +0900

    nit

--- crates/anodized-core/src/lib.rs
+++ crates/anodized-core/src/lib.rs
@@ -45,7 +45,7 @@ impl TryFrom<ContractArgs> for Contract {
                 if current_arg_order < last_order {
                     return Err(syn::Error::new(
                         arg.span(),
-                        "parameters are out of order: it must be `requires`, `maintains`, `binds`, `ensures`",
+                        "parameters are out of order: their order must be `requires`, `maintains`, `binds`, `ensures`",
                     ));
                 }
             }
