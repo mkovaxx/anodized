abbca6b07377dc29ffbaafd7d96802cae54f79ad
Mate Kovacs <mkovaxx@gmail.com>
2025-08-23 17:05:41 +0900

    make the simple test fail

--- crates/anodized-core/src/test_instrument_fn.rs
+++ crates/anodized-core/src/test_instrument_fn.rs
@@ -14,39 +14,15 @@ fn make_fn_body() -> Block {
 #[test]
 fn test_instrument_simple() {
     let contract: Contract = parse_quote! {
-        requires: x > 0,
-        ensures: output > x,
+        requires: self.is_valid(),
     };
     let body = make_fn_body();
     let is_async = false;
 
     let expected: Block = parse_quote! {
         {
-            assert!(x > 0, "Precondition failed: x > 0");
+            assert!(x > 0, "Precondition failed: self.is_valid()");
             let __anodized_output = #body;
-            assert!((|output| output > x)(__anodized_output), "Postcondition failed: | output | output > x");
-            __anodized_output
-        }
-    };
-
-    let observed = instrument_fn_body(&contract, &body, is_async).unwrap();
-    assert_block_eq(&observed, &expected);
-}
-
-#[test]
-fn test_instrument_async_simple() {
-    let contract: Contract = parse_quote! {
-        requires: x > 0,
-        ensures: output > x,
-    };
-    let body = make_fn_body();
-    let is_async = true;
-
-    let expected: Block = parse_quote! {
-        {
-            assert!(x > 0, "Precondition failed: x > 0");
-            let __anodized_output = async #body.await;
-            assert!((|output| output > x)(__anodized_output), "Postcondition failed: | output | output > x");
             __anodized_output
         }
     };
