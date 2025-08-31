c562ea175fd92a67dfc4269675d01b2afd89ff2b
Mate Kovacs <mkovaxx@gmail.com>
2025-08-31 17:59:34 +0900

    check the type of the Future

--- crates/anodized/tests/async_function.rs
+++ crates/anodized/tests/async_function.rs
@@ -10,5 +10,10 @@ async fn double_async(x: i32) -> i32 {
 
 #[test]
 fn test_async_function_compiles() {
-    let _ = double_async(5);
+    let future = double_async(5);
+
+    // We can also verify it's a future by trying to poll it (won't run due to todo!())
+    // This ensures the async transformation is correct
+    fn is_future<T: core::future::Future>(_: &T) {}
+    is_future(&future);
 }
