2373da04f54cee99bb83902a82a8e4571fb9ee43
Mate Kovacs <mkovaxx@gmail.com>
2025-08-31 18:01:33 +0900

    trim down

--- crates/anodized/tests/async_function.rs
+++ crates/anodized/tests/async_function.rs
@@ -12,8 +12,6 @@ async fn double_async(x: i32) -> i32 {
 fn test_async_function_compiles() {
     let future = double_async(5);
 
-    // We can also verify it's a future by trying to poll it (won't run due to todo!())
-    // This ensures the async transformation is correct
     fn is_future<T: core::future::Future>(_: &T) {}
     is_future(&future);
 }
