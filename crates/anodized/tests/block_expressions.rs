8516bf0b78e676ca501188d815e12ddfbafd5e10
Mate Kovacs <mkovaxx@gmail.com>
2025-09-22 00:14:20 +0900

    do not test captures on backend-nightly-contracts

--- crates/anodized/tests/block_expressions.rs
+++ crates/anodized/tests/block_expressions.rs
@@ -1,5 +1,6 @@
 use anodized::spec;
 
+#[cfg(not(feature = "backend-nightly-contracts"))]
 #[spec(
     requires: {
         // Just a longer way of writing `true` :)
@@ -17,6 +18,26 @@ use anodized::spec;
     ensures: {
         let length = vec.len();
         length > old_len
+    },
+)]
+fn function_with_blocks(vec: &mut Vec<i32>) {
+    vec.push(42);
+}
+
+#[cfg(feature = "backend-nightly-contracts")]
+#[spec(
+    requires: {
+        // Just a longer way of writing `true` :)
+        let x = 5;
+        x > 0
+    },
+    maintains: {
+        let length = vec.len();
+        length < 100
+    },
+    ensures: {
+        let length = vec.len();
+        length > 0
     },
 )]
 fn function_with_blocks(vec: &mut Vec<i32>) {
