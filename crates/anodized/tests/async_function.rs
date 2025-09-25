b16968467139ff646b78ce873c543fb43a12c55c
Mate Kovacs <mkovaxx@gmail.com>
2025-09-25 21:22:19 +0900

    more renames

--- crates/anodized/tests/async_function.rs
+++ crates/anodized/tests/async_function.rs
@@ -4,13 +4,13 @@ use anodized::spec;
     requires: x.is_finite(),
     ensures: output + output == x,
 )]
-async fn half_async(x: f32) -> f32 {
+async fn async_half(x: f32) -> f32 {
     todo!()
 }
 
 #[test]
 fn async_function_compiles() {
-    let future = half_async(5.0);
+    let future = async_half(5.0);
 
     fn is_future<T: core::future::Future>(_: &T) {}
     is_future(&future);
