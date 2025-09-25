e07287b14c7371a44a13b250b0d4137faa4acd66
Máté Kovács <481354+mkovaxx@users.noreply.github.com>
2025-09-25 20:25:09 +0900

    fix: handle `return` statements in the function body (#57)
    - When instrumenting a function body, wrap the original body in a
    closure, to correctly handle any `return` statements.

--- crates/anodized/tests/execution_order.rs
+++ crates/anodized/tests/execution_order.rs
@@ -18,20 +18,68 @@ use anodized::spec;
         log.push("ensures2") == (),
     ],
 )]
-fn instrumented_function(log: &mut Vec<&'static str>) {
+fn func(log: &mut Vec<&'static str>) {
     log.push("body");
+    return;
 }
 
 #[cfg(not(feature = "backend-no-checks"))]
 #[test]
 fn test_execution_order() {
     let mut log = Vec::new();
-    instrumented_function(&mut log);
+    func(&mut log);
 
     // Verify the exact execution order
     assert_eq!(
         log,
-        vec![
+        [
+            "requires1",
+            "requires2",
+            "maintains1",
+            "maintains2",
+            "captures1",
+            "captures2",
+            "body",
+            "maintains1",
+            "maintains2",
+            "ensures1",
+            "ensures2",
+        ]
+    );
+}
+
+#[spec(
+    requires: [
+        log.push("requires1") == (),
+        log.push("requires2") == (),
+    ],
+    maintains: [
+        log.push("maintains1") == (),
+        log.push("maintains2") == (),
+    ],
+    captures: [
+        log.push("captures1") as _alias1,
+        log.push("captures2") as _alias2,
+    ],
+    ensures: [
+        log.push("ensures1") == (),
+        log.push("ensures2") == (),
+    ],
+)]
+async fn async_func(log: &mut Vec<&'static str>) {
+    log.push("body");
+    return;
+}
+
+#[cfg(not(feature = "backend-no-checks"))]
+#[test]
+fn test_async_execution_order() {
+    let mut log = Vec::new();
+    pollster::block_on(async_func(&mut log));
+
+    assert_eq!(
+        log,
+        [
             "requires1",
             "requires2",
             "maintains1",
