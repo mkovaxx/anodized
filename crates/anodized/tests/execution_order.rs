054ce113344186192326a4a001a665f8440beb7a
Mate Kovacs <mkovaxx@gmail.com>
2025-09-28 12:52:36 +0900

    add one more test

--- crates/anodized/tests/execution_order.rs
+++ crates/anodized/tests/execution_order.rs
@@ -50,6 +50,54 @@ fn execution_order() {
 
 #[spec(
     requires: [
+        log.push("requires1") != (),
+        log.push("requires2") != (),
+    ],
+    maintains: [
+        log.push("maintains1") != (),
+        log.push("maintains2") != (),
+    ],
+    captures: [
+        log.push("captures1") as _alias1,
+        log.push("captures2") as _alias2,
+    ],
+    ensures: [
+        log.push("ensures1") != (),
+        log.push("ensures2") != (),
+    ],
+)]
+fn func_all_conditions_fail(log: &mut Vec<&'static str>) {
+    log.push("body");
+    return;
+}
+
+#[cfg(feature = "backend-check-and-print")]
+#[test]
+fn execution_order_print_only() {
+    let mut log = Vec::new();
+    func_all_conditions_fail(&mut log);
+
+    // Verify the exact execution order
+    assert_eq!(
+        log,
+        [
+            "requires1",
+            "requires2",
+            "maintains1",
+            "maintains2",
+            "captures1",
+            "captures2",
+            "body",
+            "maintains1",
+            "maintains2",
+            "ensures1",
+            "ensures2",
+        ]
+    );
+}
+
+#[spec(
+    requires: [
         log.push("requires1") == (),
         log.push("requires2") == (),
     ],
