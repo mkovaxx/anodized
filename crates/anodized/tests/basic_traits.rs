4cc49a75cabea4532bc2f84fb5db45f1dff2062c
Luke Peterson <luke@infoplatter.com>
2026-01-11 22:33:00 -0700

    Updating test to also test specs on the implementation as well as on the trait definition, and to test default method implementations

--- crates/anodized/tests/basic_traits.rs
+++ crates/anodized/tests/basic_traits.rs
@@ -17,24 +17,47 @@ pub trait TestTrait {
             *output > old_val
         },
     )]
-    fn do_something(&self, x: u32) -> u32;
-}
-
-struct TestStruct;
-
-#[spec]
-impl TestTrait for TestStruct {
-    fn current(&self) -> u32 {
-        0
-    }
     fn do_something(&self, x: u32) -> u32 {
         x * 2
     }
 }
 
+struct TestStruct(u32);
+
+#[spec]
+impl TestTrait for TestStruct {
+    fn current(&self) -> u32 {
+        self.0
+    }
+    #[spec(
+        maintains: {
+            self.0 == 3
+        },
+        ensures: {
+            *output > self.0
+        },
+    )]
+    fn do_something(&self, x: u32) -> u32 {
+        x * self.0
+    }
+}
+
+struct TestStructConst;
+
+#[spec]
+impl TestTrait for TestStructConst {
+    fn current(&self) -> u32 {
+        0
+    }
+}
+
 #[test]
 fn basic_trait_test() {
-    let test = TestStruct;
+    // Tests an impl of a trait with a spec, where there is a spec on both the implementation and on the trait interface
+    let test = TestStruct(3);
+    assert_eq!(test.do_something(500), 1500);
 
+    // Tests a default method implementation coming from a trait
+    let test = TestStructConst;
     assert_eq!(test.do_something(500), 1000);
 }
