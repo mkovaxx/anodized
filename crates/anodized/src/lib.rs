56605cbfe1ced91aa92ebc27371f38b5ec00234d
Mate Kovacs <mkovaxx@gmail.com>
2026-01-25 00:20:52 +0900

    rework error reporting for unsupported items

--- crates/anodized/src/lib.rs
+++ crates/anodized/src/lib.rs
@@ -58,16 +58,23 @@ pub fn spec(args: TokenStream, input: TokenStream) -> TokenStream {
                 .instrument_trait_impl(spec, the_impl)
                 .map(|tokens| tokens.into_token_stream())
         }
-        unsupported_item => {
-            let item_type = item_to_string(&unsupported_item);
-            let msg = format!(
-                r#"The `#[spec]` attribute doesn't yet support this item: `{}`.
-If this is a problem for your use case, please open a feature
-request at https://github.com/mkovaxx/anodized/issues/new"#,
-                item_type
-            );
-            Err(syn::Error::new_spanned(unsupported_item, msg))
+        Item::Impl(ref the_impl) if the_impl.trait_.is_none() => {
+            Err(make_item_error(&item, "inherent impl"))
         }
+        Item::Const(_) => Err(make_item_error(&item, "const")),
+        Item::Enum(_) => Err(make_item_error(&item, "enum")),
+        Item::ExternCrate(_) => Err(make_item_error(&item, "extern crate")),
+        Item::ForeignMod(_) => Err(make_item_error(&item, "extern block")),
+        Item::Macro(_) => Err(make_item_error(&item, "macro")),
+        Item::Mod(_) => Err(make_item_error(&item, "mod")),
+        Item::Static(_) => Err(make_item_error(&item, "static")),
+        Item::Struct(_) => Err(make_item_error(&item, "struct")),
+        Item::TraitAlias(_) => Err(make_item_error(&item, "trait alias")),
+        Item::Type(_) => Err(make_item_error(&item, "type")),
+        Item::Union(_) => Err(make_item_error(&item, "union")),
+        Item::Use(_) => Err(make_item_error(&item, "use")),
+        Item::Verbatim(_) => Err(make_item_error(&item, "<verbatim>")),
+        _ => Err(make_item_error(&item, "<unknown>")),
     };
 
     match result {
@@ -76,23 +83,12 @@ request at https://github.com/mkovaxx/anodized/issues/new"#,
     }
 }
 
-fn item_to_string(item: &Item) -> &str {
-    match item {
-        Item::Const(_) => "const",
-        Item::Enum(_) => "enum",
-        Item::ExternCrate(_) => "extern crate",
-        Item::Fn(_) => unreachable!(),
-        Item::ForeignMod(_) => "extern block",
-        Item::Impl(_) => "impl",
-        Item::Macro(_) => "macro",
-        Item::Mod(_) => "mod",
-        Item::Static(_) => "static",
-        Item::Struct(_) => "struct",
-        Item::Trait(_) => "trait",
-        Item::TraitAlias(_) => "trait alias",
-        Item::Type(_) => "type",
-        Item::Union(_) => "union",
-        Item::Use(_) => "use",
-        _ => "<unknown>",
-    }
+fn make_item_error(item: &syn::Item, item_type: &str) -> syn::Error {
+    let msg = format!(
+        r#"The `#[spec]` attribute doesn't yet support this item: `{}`.
+If this is a problem for your use case, please open a feature
+request at https://github.com/mkovaxx/anodized/issues/new"#,
+        item_type
+    );
+    syn::Error::new_spanned(item, msg)
 }
