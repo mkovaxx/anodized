205fae4f4813e3e8fc8981e5a77e5a21269b8e09
Mate Kovacs <mkovaxx@gmail.com>
2026-01-25 18:57:03 +0900

    test violations

--- crates/anodized/tests/basic_traits.rs
+++ crates/anodized/tests/basic_traits.rs
@@ -3,7 +3,7 @@ use anodized::spec;
 #[spec]
 pub trait TestTrait {
     /// Returns a current value
-    fn current(&self) -> u32;
+    fn current(&self) -> i32;
 
     /// Has no default
     #[spec(
@@ -11,7 +11,7 @@ pub trait TestTrait {
         captures: self.current() as old_val,
         ensures: *output > old_val,
     )]
-    fn add_to(&self, x: u32) -> u32;
+    fn add_to(&self, x: i32) -> i32;
 
     /// Has a default
     #[spec(
@@ -19,25 +19,25 @@ pub trait TestTrait {
         captures: self.current() as old_val,
         ensures: *output > old_val,
     )]
-    fn mul_by(&self, x: u32) -> u32 {
+    fn mul_by(&self, x: i32) -> i32 {
         x * 2
     }
 }
 
-struct TestStruct(u32);
+struct TestStruct(i32);
 
 #[spec]
 impl TestTrait for TestStruct {
-    fn current(&self) -> u32 {
+    fn current(&self) -> i32 {
         self.0
     }
 
-    fn add_to(&self, x: u32) -> u32 {
+    fn add_to(&self, x: i32) -> i32 {
         x + self.current()
     }
 
     #[inline(never)]
-    fn mul_by(&self, x: u32) -> u32 {
+    fn mul_by(&self, x: i32) -> i32 {
         x * self.0
     }
 }
@@ -46,24 +46,38 @@ struct TestStructConst;
 
 #[spec]
 impl TestTrait for TestStructConst {
-    fn current(&self) -> u32 {
+    fn current(&self) -> i32 {
         0
     }
 
-    fn add_to(&self, x: u32) -> u32 {
+    fn add_to(&self, x: i32) -> i32 {
         x + self.current()
     }
 }
 
 #[test]
 fn should_succeed() {
-    // Tests an impl of a trait with a spec
     let test = TestStruct(3);
     assert_eq!(test.add_to(500), 503);
     assert_eq!(test.mul_by(500), 1500);
 
-    // Tests the default method implementation coming from the trait
     let test = TestStructConst;
     assert_eq!(test.add_to(500), 500);
     assert_eq!(test.mul_by(500), 1000);
+}
+
+#[cfg(feature = "runtime-check-and-panic")]
+#[test]
+#[should_panic(expected = "Precondition failed: x > 0")]
+fn should_fail_add_to() {
+    let test = TestStruct(3);
+    assert_eq!(test.add_to(0), 3);
+}
+
+#[cfg(feature = "runtime-check-and-panic")]
+#[test]
+#[should_panic(expected = "Postcondition failed: | output | * output > old_val")]
+fn should_fail_negative_mul_by() {
+    let test = TestStruct(-3);
+    assert_eq!(test.mul_by(1), 3);
 }
