c2db3ac7a8a027be87100e79ee71889632e855da
Chammika Mannakkara <chammika_mannakkara@caddi.jp>
2026-02-22 00:18:17 +0900

    fix: clippy errors

--- crates/anodized-fmt/src/collect_comments.rs
+++ crates/anodized-fmt/src/collect_comments.rs
@@ -20,29 +20,29 @@ pub(crate) fn extract_whitespace_and_comments(
     let mut last_span: Option<Span> = None;
 
     traverse_token_stream(tokens, &mut |span: Span| {
-        if let Some(last_span) = last_span {
-            if last_span.end().line != span.start().line {
-                let text = get_text_between_spans(source, last_span.end(), span.start());
-                for (idx, line) in text.lines().enumerate() {
-                    let comment = line
-                        .to_string()
-                        .split_once("//")
-                        .map(|(_, txt)| txt)
-                        .map(str::trim)
-                        .map(ToOwned::to_owned);
+        if let Some(last_span) = last_span
+            && last_span.end().line != span.start().line
+        {
+            let text = get_text_between_spans(source, last_span.end(), span.start());
+            for (idx, line) in text.lines().enumerate() {
+                let comment = line
+                    .to_string()
+                    .split_once("//")
+                    .map(|(_, txt)| txt)
+                    .map(str::trim)
+                    .map(ToOwned::to_owned);
 
-                    let line_index = last_span.end().line - 1 + idx;
+                let line_index = last_span.end().line - 1 + idx;
 
-                    // Skip empty lines at token boundaries, but keep comments
-                    if comment.is_none()
-                        && (line_index == last_span.end().line - 1
-                            || line_index == span.start().line - 1)
-                    {
-                        continue;
-                    }
-
-                    whitespace_and_comments.insert(line_index, comment);
+                // Skip empty lines at token boundaries, but keep comments
+                if comment.is_none()
+                    && (line_index == last_span.end().line - 1
+                        || line_index == span.start().line - 1)
+                {
+                    continue;
                 }
+
+                whitespace_and_comments.insert(line_index, comment);
             }
         }
         last_span = Some(span);
