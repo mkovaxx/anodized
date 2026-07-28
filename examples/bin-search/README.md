13e2ea21276b47f13db990b8a41988b13c60c3a3
Máté Kovács <481354+mkovaxx@users.noreply.github.com>
2026-07-28 23:42:45 +0900

    [examples] feat: PBT demos for `bin-search` (#167)

--- examples/bin-search/README.md
+++ examples/bin-search/README.md
@@ -1,8 +1,26 @@
 # Anodized Example: Binary Search
 
+All tests in this example, including the fuzz target, are compiled with
+Anodized's `print`, `panic`, and `try` configurations enabled by
+[`.cargo/config.toml`](.cargo/config.toml).
+
+## Property-Based Testing
+
+Run the `proptest` test suite:
+
+```sh
+cargo test --test proptest
+```
+
+Run the `quickcheck` test suite:
+
+```sh
+cargo test --test quickcheck
+```
+
 ## Fuzzing
 
-Run the fuzzing target via the following command:
+Run the fuzzing target for `cargo-fuzz` via the following command:
 
 ```sh
 cargo +nightly fuzz run fuzz_target_1 -Ztarget-applies-to-host -Zhost-config
---
+++ examples/bin-search/tests/proptest.rs
@@ -0,0 +1,64 @@
+use anodized::result::{PostError, PreError, try_call};
+
+use proptest::prelude::*;
+use proptest_derive::Arbitrary;
+
+proptest! {
+    #![proptest_config(ProptestConfig::with_cases(10_000))]
+
+    /// Generate random inputs and assert that the precondition implies the postcondition.
+    #[test]
+    fn test_spec(inputs: Inputs<i32>) {
+        // Use Anodized's `try_call!` macro to defer acting on spec violations.
+        let result = try_call! {
+            bin_search::bin_search(&inputs.seq, &inputs.value)
+        };
+        match result {
+            // Successful call.
+            Ok(_) => {},
+            // When preconditions are violated, reject the input.
+            Err(PreError(_)) => prop_assume!(false),
+            // When postconditions are violated, panic to signal a counter-example.
+            Err(PostError(output, errors)) => {
+                eprintln!("inputs:");
+                dbg!(inputs.seq);
+                dbg!(inputs.value);
+                dbg!(output);
+                panic!("postcondition failed:{errors}");
+            }
+        }
+    }
+}
+
+#[derive(Debug, Arbitrary)]
+struct Inputs<T: Ord> {
+    seq: AscendingVec<T>,
+    value: T,
+}
+
+/// Helper newtype to more efficiently generate valid inputs.
+#[derive(Debug, Clone)]
+struct AscendingVec<T>(Vec<T>);
+
+impl<T: Ord + Arbitrary> Arbitrary for AscendingVec<T> {
+    type Parameters = <Vec<T> as Arbitrary>::Parameters;
+    type Strategy = proptest::strategy::Map<<Vec<T> as Arbitrary>::Strategy, fn(Vec<T>) -> Self>;
+
+    fn arbitrary_with(args: Self::Parameters) -> Self::Strategy {
+        Vec::<T>::arbitrary_with(args).prop_map(Self::from_unsorted)
+    }
+}
+
+impl<T: Ord> AscendingVec<T> {
+    fn from_unsorted(mut values: Vec<T>) -> Self {
+        values.sort();
+        Self(values)
+    }
+}
+
+impl<T> std::ops::Deref for AscendingVec<T> {
+    type Target = Vec<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
