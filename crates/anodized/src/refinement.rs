155f0c32f608cd27b62f9dfb0e659a987c4b7f18
Mate Kovacs <mkovaxx@gmail.com>
2026-08-18 18:02:29 +0900

    split Refined::is_valid into check_pre and check_post

--- crates/anodized/src/refinement.rs
+++ crates/anodized/src/refinement.rs
@@ -1,73 +1,119 @@
 pub trait Refined {
-    fn is_valid(&self) -> bool;
+    /// Called to validate
+    fn check_pre(&self) -> bool;
+    fn check_post(&self) -> bool;
 }
 
 impl<T: Refined> Refined for &T {
-    fn is_valid(&self) -> bool {
-        <T as Refined>::is_valid(self)
+    fn check_pre(&self) -> bool {
+        <T as Refined>::check_pre(self)
+    }
+
+    fn check_post(&self) -> bool {
+        <T as Refined>::check_post(self)
     }
 }
 
 impl<T: Refined> Refined for &mut T {
-    fn is_valid(&self) -> bool {
-        <T as Refined>::is_valid(self)
+    fn check_pre(&self) -> bool {
+        <T as Refined>::check_pre(self)
+    }
+
+    fn check_post(&self) -> bool {
+        // This is intentional, not a mistake. On `&mut T`, `check_post` forwards to `check_pre`.
+        <T as Refined>::check_pre(self)
     }
 }
 
 impl<T: Refined> Refined for Option<T> {
-    fn is_valid(&self) -> bool {
+    fn check_pre(&self) -> bool {
         match self {
-            Some(inner) => inner.is_valid(),
+            Some(inner) => inner.check_pre(),
+            None => true,
+        }
+    }
+
+    fn check_post(&self) -> bool {
+        match self {
+            Some(inner) => inner.check_post(),
             None => true,
         }
     }
 }
 
 impl<T: Refined, E: Refined> Refined for Result<T, E> {
-    fn is_valid(&self) -> bool {
+    fn check_pre(&self) -> bool {
         match self {
-            Ok(okay) => okay.is_valid(),
-            Err(err) => err.is_valid(),
+            Ok(okay) => okay.check_pre(),
+            Err(err) => err.check_pre(),
+        }
+    }
+
+    fn check_post(&self) -> bool {
+        match self {
+            Ok(okay) => okay.check_post(),
+            Err(err) => err.check_post(),
         }
     }
 }
 
 impl<T: Refined> Refined for Box<T> {
-    fn is_valid(&self) -> bool {
-        self.as_ref().is_valid()
+    fn check_pre(&self) -> bool {
+        self.as_ref().check_pre()
+    }
+
+    fn check_post(&self) -> bool {
+        self.as_ref().check_post()
     }
 }
 
 impl Refined for () {
-    fn is_valid(&self) -> bool {
+    fn check_pre(&self) -> bool {
+        true
+    }
+
+    fn check_post(&self) -> bool {
         true
     }
 }
 
 impl<T1: Refined> Refined for (T1,) {
-    fn is_valid(&self) -> bool {
-        self.0.is_valid()
+    fn check_pre(&self) -> bool {
+        self.0.check_pre()
+    }
+
+    fn check_post(&self) -> bool {
+        self.0.check_post()
     }
 }
 
 impl<T1: Refined, T2: Refined> Refined for (T1, T2) {
-    fn is_valid(&self) -> bool {
-        self.0.is_valid() && self.1.is_valid()
+    fn check_pre(&self) -> bool {
+        self.0.check_pre() && self.1.check_pre()
+    }
+
+    fn check_post(&self) -> bool {
+        self.0.check_post() && self.1.check_post()
     }
 }
 
 impl<T1: Refined, T2: Refined, T3: Refined> Refined for (T1, T2, T3) {
-    fn is_valid(&self) -> bool {
-        self.0.is_valid() && self.1.is_valid() && self.2.is_valid()
+    fn check_pre(&self) -> bool {
+        self.0.check_pre() && self.1.check_pre() && self.2.check_pre()
+    }
+
+    fn check_post(&self) -> bool {
+        self.0.check_post() && self.1.check_post() && self.2.check_post()
     }
 }
 
-/// Implement `Refined` for concrete types, with `is_valid` always `true`.
+/// Implement `Refined` for concrete types, with `check_pre/post` always `true`.
 macro_rules! tautological_refinement {
     ($($ty:ty),+ $(,)?) => {
         $(
             impl $crate::refinement::Refined for $ty {
-                fn is_valid(&self) -> bool { true }
+                fn check_pre(&self) -> bool { true }
+                fn check_post(&self) -> bool { true }
             }
         )+
     };
