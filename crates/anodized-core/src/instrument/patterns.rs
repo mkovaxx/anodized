9e57312c05c11133b84056c7e0e332da498fdf20
Mate Kovacs <mkovaxx@gmail.com>
2026-08-28 15:01:08 +0900

    improve wording

--- crates/anodized-core/src/instrument/patterns.rs
+++ crates/anodized-core/src/instrument/patterns.rs
@@ -57,7 +57,7 @@ pub fn tame_pattern(id_gen: &mut IdentGenerator, mut pat: Pat) -> syn::Result<Ta
         let message = if has_rest {
             "inside `#[spec]`, patterns containing `..` must bind only by `ref`"
         } else {
-            "inside `#[spec]`, patterns cannot mix `move` and `ref` bindings"
+            "inside `#[spec]`, patterns cannot mix moving and `ref` bindings"
         };
         Err(syn::Error::new_spanned(pat, message))
     }
